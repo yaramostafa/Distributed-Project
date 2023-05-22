93c165226bb67989efd5c909f9d83801354fa5cf
yara <89746218+yaramostafa@users.noreply.github.com>
2023-05-22 20:25:25 +0300

    updated multiplayer

--- cargame.py
+++ cargame.py
@@ -1,55 +1,54 @@
 import random
 from time import sleep
-
 import pygame
+
 class CarRacing:
     def __init__(self):
 
         pygame.init()
-        self.display_width = 800
+        self.display_width = 1300
         self.display_height = 600
         self.black = (0, 0, 0)
+        self.red = (255,0,0)
         self.white = (255, 255, 255)
         self.clock = pygame.time.Clock()
         self.gameDisplay = None
-        self.initialize()
 
-        # To add players
-        self.players = []
-        self.cars = [pygame.image.load('.\\img\\car.png'), pygame.image.load('.\\img\\enemy_car_2.png'),
-                     pygame.image.load('.\\img\\car2.png')]
-        num_players = int(input("Enter the number of players: "))
-        for i in range(num_players):
-            player_name = input("Enter player {} name: ".format(i + 1))
-            self.players.append(player_name)
+        self.initialize()
 
     def initialize(self):
 
         self.crashed = False
 
         self.carImg = pygame.image.load('.\\img\\car.png')
-        self.car_x_coordinate = (self.display_width * 0.45)
+        self.car_x_coordinate = (self.display_width * 0.6)
         self.car_y_coordinate = (self.display_height * 0.8)
         self.car_width = 49
 
         # enemy_car
         self.enemy_car = pygame.image.load('.\\img\\enemy_car_1.png')
-        self.enemy_car_startx = random.randrange(310, 450)
+        self.enemy_car_startx = random.randrange(50, 950)
         self.enemy_car_starty = -600
-        self.enemy_car_speed = 3
+        self.enemy_car_speed = 5
         self.enemy_car_width = 49
         self.enemy_car_height = 100
 
+        # enemy_car2
+        self.enemy_car2 = pygame.image.load('.\\img\\enemy_car_2.png')
+        self.enemy_car2_startx = random.randrange(50, 950)
+        self.enemy_car2_starty = -600
+        self.enemy_car2_speed = 5
+        self.enemy_car2_width = 49
+        self.enemy_car2_height = 100
+
         # Background
-        self.bgImg = pygame.image.load(".\\img\\back_ground.jpg")
-        self.bg_x1 = (self.display_width / 2) - (360 / 2)
-        self.bg_x2 = (self.display_width / 2) - (360 / 2)
+        self.bgImg = pygame.image.load(".\\img\\back_ground1.png")
+        self.bg_x1 = 0#(self.display_width / 2) - (1046 / 2)
+        self.bg_x2 = 0 #(self.display_width / 2) - (1046 / 2)
         self.bg_y1 = 0
         self.bg_y2 = -600
         self.bg_speed = 3
         self.count = 0
-
-
 
     def car(self, car_x_coordinate, car_y_coordinate):
         self.gameDisplay.blit(self.carImg, (car_x_coordinate, car_y_coordinate))
@@ -61,7 +60,6 @@ class CarRacing:
 
     def run_car(self):
         while not self.crashed:
-
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.crashed = True
@@ -77,29 +75,40 @@ class CarRacing:
                     print ("x: {x}, y: {y}".format(x=self.car_x_coordinate, y=self.car_y_coordinate))
 
             self.gameDisplay.fill(self.black)
-            self.back_ground_raod()
+            self.back_ground_road()
 
             self.run_enemy_car(self.enemy_car_startx, self.enemy_car_starty)
+            self.run_enemy_car2(self.enemy_car2_startx, self.enemy_car2_starty)
+
             self.enemy_car_starty += self.enemy_car_speed
+            self.enemy_car2_starty += self.enemy_car_speed
 
             if self.enemy_car_starty > self.display_height:
                 self.enemy_car_starty = 0 - self.enemy_car_height
-                self.enemy_car_startx = random.randrange(310, 450)
+                self.enemy_car_startx = random.randrange(50, 950)
+
+            if self.enemy_car2_starty > self.display_height:
+                self.enemy_car2_starty = 0 - self.enemy_car2_height
+                self.enemy_car2_startx = random.randrange(50, 950)
 
             self.car(self.car_x_coordinate, self.car_y_coordinate)
             self.highscore(self.count)
             self.count += 1
             if (self.count % 100 == 0):
-                self.enemy_car_speed += 1
-                self.bg_speed += 1
-            if self.car_y_coordinate < self.enemy_car_starty + self.enemy_car_height:
+                # self.enemy_car_speed += 1
+                # self.bg_speed += 1
+                print("Milestone Reached!")
+            if self.car_y_coordinate < self.enemy_car_starty + self.enemy_car_height or self.car_y_coordinate < self.enemy_car2_starty + self.enemy_car2_height:
                 if self.car_x_coordinate > self.enemy_car_startx and self.car_x_coordinate < self.enemy_car_startx + self.enemy_car_width or self.car_x_coordinate + self.car_width > self.enemy_car_startx and self.car_x_coordinate + self.car_width < self.enemy_car_startx + self.enemy_car_width:
                     self.crashed = True
                     self.display_message("Game Over !!!")
+                if self.car_x_coordinate > self.enemy_car2_startx and self.car_x_coordinate < self.enemy_car2_startx + self.enemy_car2_width or self.car_x_coordinate + self.car_width > self.enemy_car2_startx and self.car_x_coordinate + self.car_width < self.enemy_car2_startx + self.enemy_car2_width:
+                    self.crashed = True
+                    self.display_message("Game Over !!!")
 
-            if self.car_x_coordinate < 310 or self.car_x_coordinate > 460:
-                self.crashed = True
-                self.display_message("Game Over !!!")
+            if self.car_x_coordinate < 50 or self.car_x_coordinate > 950:
+                 self.crashed = True
+                 self.display_message("Game Over !!!")
 
             pygame.display.update()
             self.clock.tick(60)
@@ -107,21 +116,15 @@ class CarRacing:
     def display_message(self, msg):
         font = pygame.font.SysFont("comicsansms", 72, True)
         text = font.render(msg, True, (255, 255, 255))
-        self.gameDisplay.blit(text, (400 - text.get_width() // 2, 240 - text.get_height() // 2))
+        self.gameDisplay.blit(text, (550 - text.get_width() // 2, 240 - text.get_height() // 2))
         self.display_credit()
-
-        for i, player in enumerate(self.players):
-            score_text = "Score for {}: {}".format(player, self.count)
-            score_text_surface = font.render(score_text, True, self.white)
-            self.gameDisplay.blit(score_text_surface, (400 - score_text_surface.get_width() // 2, 300 + i * 30))
-
         pygame.display.update()
         self.clock.tick(60)
         sleep(1)
         car_racing.initialize()
         car_racing.racing_window()
 
-    def back_ground_raod(self):
+    def back_ground_road(self):
         self.gameDisplay.blit(self.bgImg, (self.bg_x1, self.bg_y1))
         self.gameDisplay.blit(self.bgImg, (self.bg_x2, self.bg_y2))
 
@@ -137,22 +140,21 @@ class CarRacing:
     def run_enemy_car(self, thingx, thingy):
         self.gameDisplay.blit(self.enemy_car, (thingx, thingy))
 
-    def highscore(self, count):
-        font = pygame.font.SysFont("arial", 20)
-        #text = font.render("Score : " + str(count), True, self.white)
-        #self.gameDisplay.blit(text, (0, 0))
-        #ont = pygame.font.SysFont("arial", 20)
-        for i, player in enumerate(self.players):
-            text = font.render("{}: {}".format(player, count), True, self.white)
-            self.gameDisplay.blit(text, (0, i * 20))
+    def run_enemy_car2(self, thingx, thingy):
+        self.gameDisplay.blit(self.enemy_car2, (thingx, thingy))
 
+    def highscore(self, count):
+        font = pygame.font.SysFont("arial", 22)
+        text = font.render("Score : " + str(count), True, self.white)
+        self.gameDisplay.blit(text, (0, 0)) #65
 
     def display_credit(self):
         font = pygame.font.SysFont("lucidaconsole", 14)
         text = font.render("Thanks for playing!", True, self.white)
-        self.gameDisplay.blit(text, (600, 520))
+        self.gameDisplay.blit(text, (850, 560))
 
 
 if __name__ == '__main__':
     car_racing = CarRacing()
     car_racing.racing_window()
+
