63a644734af0e33bcf7327a58d46d85856731e7c
Nasef <iamnasef@gmail.com>
2023-06-17 05:52:04 -0400

    testing asg

--- infrastructure/data.sh
+++ infrastructure/data.sh
@@ -6,5 +6,6 @@ pip3 install mysqlclient
 yum install git -y
 TOKEN="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"
 cd /home/ec2-user && git clone https://$TOKEN@github.com/yaramostafa/Distributed-Project.git
+python3 -m http.server 1337 &
 python3 /home/ec2-user/Distributed-Project/server.py
-python3 -m http.server 1337
+
