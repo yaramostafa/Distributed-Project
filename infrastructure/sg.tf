95d11065e78ba18b6c175ffbcc4702f23cab7d9c
Nasef <iamnasef@gmail.com>
2023-06-17 06:14:02 -0400

    change server.py

--- infrastructure/sg.tf
+++ infrastructure/sg.tf
@@ -4,41 +4,13 @@ resource "aws_security_group" "cars-dev-securitygroup" {
   vpc_id = "${aws_vpc.cars-dev-vpc.id}"
 
   ingress {
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
     cidr_blocks = ["${var.all-cidr}"]
   }
 
-    ingress {
-    from_port = 10001
-    to_port = 10001
-    protocol = "tcp"
-    cidr_blocks = ["${var.all-cidr}"]
-  }
-
-    ingress {
-    from_port = 3306
-    to_port = 3306
-    protocol = "tcp"
-    cidr_blocks = ["${var.all-cidr}"]
-  }
-
-    ingress {
-    from_port = 443
-    to_port = 443
-    protocol = "tcp"
-    cidr_blocks = ["${var.all-cidr}"]
-  }
-
-    ingress {
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
-    cidr_blocks = ["${var.all-cidr}"]
-  }
-
-  egress{
+    egress {
     from_port = 0
     to_port = 0
     protocol = "-1"
