8477962c5196a3ca41b93b5b226b3b75b9b18e1e
Nasef <iamnasef@gmail.com>
2023-06-18 18:24:53 -0400

    adding token

--- infrastructure/data.sh
+++ infrastructure/data.sh
@@ -4,7 +4,7 @@ yum update -y
 yum install python3 -y
 pip3 install mysqlclient
 yum install git -y
-TOKEN="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"
+TOKEN="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"
 cd /home/ec2-user && git clone https://$TOKEN@github.com/yaramostafa/Distributed-Project.git
 python3 -m http.server 1337 &
 python3 /home/ec2-user/Distributed-Project/ChatServer.py &
