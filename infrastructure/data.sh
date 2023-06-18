fb164b269d329de1296e9e5e96750fccf6e81d88
Nasef <iamnasef@gmail.com>
2023-06-18 18:44:54 -0400

    fixing mysql bug on production

--- infrastructure/data.sh
+++ infrastructure/data.sh
@@ -2,7 +2,9 @@
 
 yum update -y
 yum install python3 -y
-pip3 install mysqlclient
+dnf install -y pip
+dnf install -y mariadb105-devel gcc python3-devel
+pip install mysqlclient
 yum install git -y
 TOKEN="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"
 cd /home/ec2-user && git clone https://$TOKEN@github.com/yaramostafa/Distributed-Project.git
