ea600b3c62c3016937c0dc59ef9e351fb986f5e9
yara <89746218+yaramostafa@users.noreply.github.com>
2023-06-19 17:17:45 +0300

    Final version fixed minor bugs

--- CarClient.py
+++ CarClient.py
@@ -124,11 +124,8 @@ class CarRacing:
                 if (event.type == pygame.KEYDOWN):
                     if (event.key == pygame.K_LEFT):
                         self.player[4] -= 50
-                        #print("CAR X COORDINATES: %s" % self.player[4])
                     if (event.key == pygame.K_RIGHT):
                         self.player[4] += 50
-                        #print("CAR X COORDINATES: %s" % self.player[4])
-                    #print("x: {x}, y: {y}".format(x=self.player[4], y=self.player[5]))
 
             self.gameDisplay.fill(self.black)
             self.back_ground_road()
@@ -149,8 +146,6 @@ class CarRacing:
 
 
             self.opponents = self.client.send(self.player)
-            print('Me: '+str(self.player[0]))
-            print('Opp:'+str(self.opponents))
             self.drawOpp()
             self.car(self.player[4], self.player[5])
 
